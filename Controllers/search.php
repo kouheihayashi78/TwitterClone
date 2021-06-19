a51f5fab6ce13f0e4db1b30b5b085b2bf1fce537
kouheihayashi78 <kouhei.basketball7.8@gmail.com>
2021-06-19 15:51:06 +0900

    検索機能実装

---
+++ Controllers/search.php
@@ -0,0 +1,31 @@
+<?php
+////////////////////
+// サーチコントローラー
+////////////////////
+// 設定を読み込み
+include_once '../config.php';
+
+// 便利な関数を読みこみ
+include_once '../util.php';
+
+// ツイートデータ操作モデルを読み込む
+include_once '../Models/tweets.php';
+
+// ログインしているか
+$user = getUserSession();
+if (!$user) {
+    // ログインしていない
+    header('Location' . HOME_URL . 'Controllers/sign-in.php');
+    exit;
+}
+// 検索キーワードを取得
+$keyword = null; // ここでデフォルトを設定してないとエラーになる。セットしておく。
+if (isset($_GET['keyword'])) {
+    $keyword = $_GET['keyword'];
+}
+// 画面表示
+$view_user = $user;
+$view_keyword = $keyword;
+//ツイート一覧
+$view_tweets = findTweets($user, $keyword);
+include_once '../views/search.php';
